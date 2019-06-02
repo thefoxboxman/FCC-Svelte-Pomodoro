a7ef99979037e267d6eb20ed6eebb09b08d81dec
F John De Costa <john@mayerfleur.com>
2019-06-03 08:14:28 +1000

    start

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,17 @@
-<script>
-  let name = "Max";
-  let age = 62;
 
-  function increaseAge() {
-    age += 1;
-  }
+
+
+<script>
+
+	
+	
 </script>
 
 <style>
-  h1 {
-    color: rgb(28, 114, 190);
-    border: 2px solid black;
-  }
-
-  button {
-    background-color: bisque;
-  }
+  
 </style>
+<h1>Pomodoro Clock</h1>
+<label  id='break-label' for="break-length"></label>
 
-<h1>Hello {name}, your age is {age}!</h1>
-<button on:click="{increaseAge}">Change age</button>
+		
+		
