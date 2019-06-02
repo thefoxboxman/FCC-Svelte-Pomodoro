05b28812da5740676b9322b8df7faa9c6b7fad45
F John De Costa <john@mayerfleur.com>
2019-06-02 19:24:20 +1000

    Initial Commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
-	export let name;
+  let name = "Max";
+  let age = 62;
+
+  function increaseAge() {
+    age += 1;
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: rgb(28, 114, 190);
+    border: 2px solid black;
+  }
+
+  button {
+    background-color: bisque;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {name}, your age is {age}!</h1>
+<button on:click="{increaseAge}">Change age</button>
